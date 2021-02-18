095bcc2a595cb90255892f16f543ee146e870cc1
Pratheepa Jeganathan <jpratheepa31@gmail.com>
2021-02-18 01:05:11 -0500

    creating articles

--- R/betaAligned.R
+++ R/betaAligned.R
@@ -1,23 +1,32 @@
-betaAligned <- function(beta, 
-                         K, 
-                         aligned, 
-                         iter = 2000, 
+#' Align ASVs proportions in topics across chain.
+#'
+#' @param beta array. three dimensional array (iterations * topic * ASVs).
+#' @param aligned matrix. A matrix of topic assignment for each chain. output of alignmentMatrix.
+#' @inheritParams alignmentMatrix
+#'
+#' @return array. three dimensional array
+#' @export
+#'
+betaAligned <- function(beta,
+                         K,
+                         aligned,
+                         iter = 2000,
                          chain = 4){
   # align topics between chains
   # no need to switch array dimnension
   beta_chain <- list()
   beta_chain[[1]] <- beta[1:(iter/2), ,]
   beta_chain[[1]] <- beta_chain[[1]][, aligned[,1], ]
-  
+
   for(ch in 2:chain){
     beta_chain[[ch]] <- beta[((ch-1)*(iter/2)+1):(ch*(iter/2)),,]
     beta_chain[[ch]] <- beta_chain[[ch]][,aligned[,ch],]
   }
-  
+
   beta_aligned <- abind(beta_chain[[1]], beta_chain[[2]], along = 1)
   for(ch in 3:chain){
     beta_aligned <- abind(beta_aligned, beta_chain[[ch]], along = 1)
   }
-  
+
   return(beta_aligned)
 }
--- R/phyloseqTransformation.R
+++ R/phyloseqTransformation.R
@@ -1,62 +1,69 @@
-phyloseqTransformation <- function(ps, 
-                                   c = .4){
+#' Anscombe (1948) transformation
+#'
+#' @param cc Double. a constant used in the Anscombe (1948) transformation if dispersion is <= 2
+#' @inheritParams alignmentMatrix
+#' @return phyloseq object. otu table has transformed values of counts.
+#' @export
+#'
+phyloseqTransformation <- function(ps,
+                                   cc = .4){
   dds <- phyloseq_to_deseq2(ps, design = ~ 1)
   dds <-  estimateSizeFactors(dds, type = "poscounts")
   dds <- estimateDispersions(dds, fitType = "local")
   abund <- assay(dds)
   abund_temp <- matrix(nrow = nrow(abund), ncol = ncol(abund))
-  
+
   # Column co-factor
-  
+
   for(i in 1:dim(abund)[1]){
     for(j in 1:dim(abund)[2]){
       abund_temp[i,j] <- abund[i,j]/sizeFactors(dds)[j]
     }
   }
-  
-  
+
+
   # Row co-factor
-  
+
   # k > 2
   ind <- which(dispersions(dds) > 2)
   # K <= 2
   ind2 <- which(dispersions(dds) <= 2)
-  
+
   for(i in ind){
     for(j in 1:dim(abund_temp)[2]){
       if(abund_temp[i,j] > 0){
-        abund_temp[i,j] <- sqrt(dispersions(dds)[i]- .5)*asinh(sqrt((abund_temp[i, j]+ c)/(dispersions(dds)[i]-2*c)))
+        abund_temp[i,j] <- sqrt(dispersions(dds)[i]- .5)*asinh(sqrt((abund_temp[i, j]+ cc)/(dispersions(dds)[i]-2*cc)))
       }else{
         abund_temp[i,j] <- sqrt(dispersions(dds)[i]- .5)*asinh(sqrt(abund_temp[i, j]))
       }
-      
+
     }
   }
-  
-  
+
+
   if(length(ind2) > 0){
     for(i in ind2){
       for(j in 1:dim(abund_temp)[2]){
         if(abund_temp[i,j] > 0){
           abund_temp[i,j] <- log2(abund_temp[i, j] + .5*dispersions(dds)[i])
         }
-        
+
       }
     }
   }
-  
-  
+
+
   rownames(abund_temp) <- rownames(abund)
   colnames(abund_temp) <- colnames(abund)
-  
-  
+
+
   ps <- phyloseq(otu_table(abund_temp, taxa_are_rows = TRUE),
-                 sample_data(ps), 
-                 tax_table(ps), 
+                 sample_data(ps),
+                 tax_table(ps),
                  phy_tree(ps))
-  
+
   return(ps)
-  
+
 }
 
 
@@ -64,34 +71,3 @@ phyloseqTransformation <- function(ps,
 
 
 
-# phyloseqTransformation <- function(ps, 
-#                                   c = .4){
-#   dds <- phyloseq_to_deseq2(ps, design = ~ 1)
-#   dds = estimateSizeFactors(dds, type = "poscounts")
-#   dds = estimateDispersions(dds, fitType = "local")
-#   abund = assay(dds)
-#   abund_temp = sweep(abund, MARGIN = 2, sizeFactors(dds), "/")
-#   
-#   abund[abund > 0] = abund + c
-#   den = dispersions(dds)-2*c
-#   abund_temp = sweep(abund_temp, MARGIN = 1, den, "/")
-#   # k > 2
-#   ind = which(dispersions(dds) > 2)
-#   abund_temp[ind, ] = sqrt(dispersions(dds)[ind]- .5)*asinh(sqrt(abund_temp[ind, ]))
-#   #abund_temp[ind, ] = asinh(sqrt(abund_temp[ind, ]))
-#   # K <= 2
-#   ind2 = which(dispersions(dds) <= 2)
-#   if(length(ind2) > 0){
-#     abund_temp[ind2, ][abund_temp[ind2, ] > 0] = log(abund_temp[ind2, ] + .5*dispersions(dds)[ind2])
-#   }
-#   
-#   ps <- phyloseq(otu_table(abund_temp, taxa_are_rows = TRUE),
-#                  sample_data(ps), 
-#                  tax_table(ps), 
-#                  phy_tree(ps))
-#   
-#   return(ps)
-#   
-# }
-# 
-# 
