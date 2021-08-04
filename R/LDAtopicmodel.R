7e5415a9a9c4129b43024bb100d954a192d3dc58
Pratheepa Jeganathan <jpratheepa31@gmail.com>
2021-08-03 23:16:58 -0400

    rewrite vignette

---
+++ R/LDAtopicmodel.R
@@ -0,0 +1,18 @@
+#' Bayesian latent Dirichlet allocation for 16S and metagenomics data.
+#'
+#' @param stan_data A list of stan data.
+#' @param ... Arguments passed to `rstan::sampling` (e.g. iter, chains).
+#'
+#' @return An object of class `stanfit` returned by `rstan::sampling`
+#' @export
+LDAtopicmodel <- function(stan_data, ...){
+        stan.data <- stan_data
+        stan.fit <- rstan::sampling(
+          stanmodels$lda,
+          data = stan_data,
+          ...
+        )
+        return(stan.fit)
+}
+
+
